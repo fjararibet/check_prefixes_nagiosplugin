8821149da807125cedaf3bbf644ac2ce43436f6a
Felipe Jara <fjararibet@gmail.com>
2023-01-26 18:15:35 -0300

    probe method returns a metric per peer

--- check_all_prefixes.py
+++ check_all_prefixes.py
@@ -15,28 +15,28 @@ class Prefixes(nagiosplugin.Resource):
 
     """
 
-    def __init__(self, peer_ip):
-        self.peer_ip = peer_ip
+    def __init__(self):
+        self.__bgp_summary = sp.check_output('sudo vtysh -c "show ip bgp summary"', shell=True, text=True)
 
-    def prefixes(self):
+    def prefixes(self, peer_ip):
 
         # sudo vtysh -c "show ip bgp summary" | grep {$peer_ip}
         try:
-            bgp_summary = sp.Popen(['sudo', 'vtysh', '-c', "show ip bgp summary"], stdout=sp.PIPE)
-            peer_line = sp.Popen(["grep", "-w", self.peer_ip], stdin=bgp_summary.stdout, stdout=sp.PIPE)
+            bgp_summary = sp.Popen(['echo', self.__bgp_summary], stdout=sp.PIPE)
+            peer_line = sp.Popen(["grep", "-w", peer_ip], stdin=bgp_summary.stdout, stdout=sp.PIPE)
             bgp_summary.stdout.close()
             peer_data = peer_line.stdout.read().split()
             prefixes = int(peer_data[9])
         except IndexError:
             raise nagiosplugin.CheckError(
-                "Cannot determine the number of Prefixes Received, try indicating a peer with -p")   
+                "Cannot determine the number of Prefixes Received, no peers found.")   
         except Exception:   
             raise nagiosplugin.CheckError(
                 '''Cannot determine the number of Prefixes Received using 'vtysh -c "show ip bgp summary"'.''')
 
         
         try:
-            bgp_neighbors = sp.Popen(['sudo', 'vtysh', '-c', "show ip bgp neighbors", self.peer_ip], stdout=sp.PIPE)
+            bgp_neighbors = sp.Popen(['sudo', 'vtysh', '-c', "show ip bgp neighbors", peer_ip], stdout=sp.PIPE)
             grep_peer = sp.Popen(['grep', 'Local host:'], stdin=bgp_neighbors.stdout, stdout=sp.PIPE)
             bgp_neighbors.stdout.close()
             awk = sp.Popen(['awk', '-F', '[," "]', '{print $3}'] , stdin=grep_peer.stdout, stdout=sp.PIPE)
@@ -44,14 +44,14 @@ class Prefixes(nagiosplugin.Resource):
             host_ip = awk.stdout.read().strip()
         except OSError:
             raise nagiosplugin.CheckError(
-                f'''Cannot determine the number of Prefixes Received using 'vtysh -c "show ip bgp neighbors {self.peer_ip}" "''')
+                f'''Cannot determine the number of Prefixes Received using 'vtysh -c "show ip bgp neighbors {peer_ip}" "''')
         except AttributeError:
             raise nagiosplugin.CheckError(
-                f'''Cannot determine the number of Prefixes Received using 'vtysh -c "show ip bgp neighbors {self.peer_ip},
+                f'''Cannot determine the number of Prefixes Received using 'vtysh -c "show ip bgp neighbors {peer_ip},
                  peer might be out of service." "''')
         
         db = DB_bgp()
-        max_prefixes = db.max_PfxRcd(host_ip, self.peer_ip)
+        max_prefixes = db.max_PfxRcd(host_ip, peer_ip)
 
         ratio = prefixes / max_prefixes
         
@@ -59,7 +59,24 @@ class Prefixes(nagiosplugin.Resource):
     
     # Returns a Metric nagiosplugin object with the prefixes information
     def probe(self):
-        metric = nagiosplugin.Metric("prefixes proportion", self.prefixes(), uom="%")
+        bgp_summary = sp.Popen(['echo', self.__bgp_summary], stdout=sp.PIPE)
+        grep_total_neighbors = sp.Popen(['grep', "Total number of neighbors"], stdin=bgp_summary.stdout, stdout=sp.PIPE )
+        bgp_summary.stdout.close()
+        tail = sp.Popen(['tail', '-c', '+27'], stdin=grep_total_neighbors.stdout, stdout=sp.PIPE)
+        grep_total_neighbors.stdout.close()
+        total_neighbors = int(tail.stdout.read())
+
+        peers = sp.check_output(
+            f'''sudo vtysh -c "show ip bgp summary" | grep -A {total_neighbors} -w Neighbor | tail -n +2 ''',
+            shell=True, text=True)
+        peers = peers.split('\n', total_neighbors - 1)
+
+        metric = []
+        for line in peers:
+            peer_data = line.split()
+            peer_ip = peer_data[0]
+            metric += [nagiosplugin.Metric("prefixes proportion", self.prefixes(peer_ip), uom="%")]
+
         return metric
     
 
@@ -78,7 +95,7 @@ def main():
 
     args = argp.parse_args()
     check = nagiosplugin.Check(
-        Prefixes(args.peer),
+        Prefixes(),
         nagiosplugin.ScalarContext("prefixes proportion", args.warning, args.critical))
     check.main()
 
